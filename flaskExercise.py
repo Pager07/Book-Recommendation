14d98c34d4029a893618a1e6bc6e602555e3c6c1
Sandeep <sunthapa1@hotmail.com>
2019-11-05 19:56:05 +0000

    flask excerise complete

--- flaskExercise.py
+++ flaskExercise.py
@@ -1,11 +1,28 @@
-from flask import Flask
+# initial setup of Flask
+from flask import Flask, render_template, request, redirect, Response
+import random, json
+
 app = Flask(__name__)
 
-@app.route("/output")
+# setup endpoint
+@app.route("/")
 def output():
-    return "Hello World"
+    return render_template("index.html", name="Joe")
 
+
+#setup endpoint
+@app.route('/receiver', methods = ['POST'])
+def worker():
+    # read json + reply
+    data = request.get_json(force = True)
+    result = ''
+    print(data)
+    # for item in data:
+    #     # loop over every row
+    #     result += str(item['name']) + '\n'
+    result += data['name']
+    return result
+
+# setup your script to actually run when it is requested:
 if __name__ == '__main__':
     app.run()
-
-from flask import Flask
